145187e177527b41a22488be7ceb34d6e1343fee
MSmith <marie.ellen@gmail.com>
2022-10-04 10:46:06 -0400

    created favorite button
    Created favorite button. It appears in the DOM with the rest of the art information. Disappears on page refresh.

--- script.js
+++ script.js
@@ -1,77 +1,94 @@
 // TO DO:
   // 
-  // Add an 'any' choice.
-  // Option to star the favorites. Favorites are put in localStorage.
+  // Get 'any' choice to return proper departmentID value.
   // Favorites are listed in the DOM, along with art info PLUS Department & Met url.
-  // Style the page!
+  // Favorites are kept in localStorage for next session.
+  // Style the page - custom background for each dept.
     
 
 
   document.querySelector('button').addEventListener('click', showArt);
-  
+
+  document.querySelector('.favoriteButton').addEventListener('click', addToFav);
   
   function showArt() {
     let departmentID = document.querySelector('select').value;
 
     if (departmentID == 'random') {
-      departmentID == (Math.floor(Math.random()*21));
-      return departmentID;
+      let departmentID = (Math.ceil(Math.random()*21));
+
+      if (departmentID == '2' || departmentID == '20') {
+        let departmentID = '7';
+      }
+    } else {
+    let departmentID = document.querySelector('select').value;
     }
+
 
     console.log(departmentID)
 
-  // // Select randomly what piece of art to show on DOM, based on department selected. 
-  //     fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${departmentID}&q=art`) 
-  //           .then(res => res.json())
-  //           .then(data => {
-  //               console.log(data)
-  //               choice = data.objectIDs[Math.floor(Math.random()*(data.objectIDs.length))];
-  //               console.log(choice)
+  // // Select randomly piece of art, based on department selected. 
+      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${departmentID}&q=art`) 
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                let choice = data.objectIDs[Math.floor(Math.random()*(data.objectIDs.length))];
+                console.log(choice)
 
-  //               fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${choice}`) 
-  //                   .then(res => res.json())
-  //                   .then(data => {
-  //                       console.log(data)
+// Show the selected art's picture and info in the DOM, pulled from the 2nd API link:
+                fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${choice}`) 
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
 
-  //                       document.querySelector('h3').innerText = 'Title: ' +data.title
+                        document.querySelector('h3').innerText = 'Title: ' +data.title
 
-  //                       document.querySelector('h4').innerText = 'Artist: '+data.artistAlphaSort
+                        document.querySelector('h4').innerText = 'Artist: '+data.artistAlphaSort
 
-  //                       document.querySelector('#date').innerText = 'Date: '+data.objectDate
+                        document.querySelector('#date').innerText = 'Date: '+data.objectDate
 
-  //                       document.querySelector('#period').innerText = 'Period: ' +data.period
+                        document.querySelector('#period').innerText = 'Period: ' +data.period
 
-  //                       function imageAvailable() {
-  //                         document.querySelector('#noImage').innerText = "";
-  //                         document.querySelector('img').src = "";
+                        function imageAvailable() {
+                          document.querySelector('#noImage').innerText = "";
+                          document.querySelector('img').src = "";
 
-  //                         if (data.primaryImageSmall === "") {
-  //                           document.querySelector('#noImage').innerText = 'Image currently not available'
+                          if (data.primaryImageSmall === "") {
+                            document.querySelector('#noImage').innerText = 'Image currently not available'
                             
-  //                         }else {
-  //                           document.querySelector('img').src = data.primaryImageSmall
-  //                         };
-  //                       }
+                          }else {
+                            document.querySelector('img').src = data.primaryImageSmall
+                          };
+                        }
 
-  //                       imageAvailable()
+                        imageAvailable();
+
+// make favorite button visible
+                        
+                        const favorite = document.querySelector('.favoriteButton')
+
+                        function displayFavorite () {
+                          favorite.classList.remove('hidden')
+                        }
+                        
+                        displayFavorite();
                       
-  //                   })
+                    })
             
-  //             .catch(err => {
-  //                 console.log(`error ${err}`)
-  //             })
+              .catch(err => {
+                  console.log(`error ${err}`)
+              })
                 
             
                 
-  //           })
+            })
             
-  //             .catch(err => {
-  //                 console.log(`error ${err}`)
-  //             });
-    
-     
-  
-  
-    
-  
+            .catch(err => {
+              console.log(`error ${err}`)
+          });
   };
+
+
+  function addToFav() {
+    console.log('It works!!')
+  }
