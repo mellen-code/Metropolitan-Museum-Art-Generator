f0bd1ca4ca3c2b050940c1f7631acd9a5c2b7a87
MSmith <marie.ellen@gmail.com>
2023-04-15 17:39:40 -0400

    Trying to add and activate React.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+// import { createRoot } from 'react-dom/client';
+
+// // Clear the existing HTML content
+// document.body.innerHTML = '<div id="app"></div>';
+
+// // Render your React component instead
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, world</h1>);
+
+
+
+
 document.querySelector('button').addEventListener('click', showArt);
 
   
